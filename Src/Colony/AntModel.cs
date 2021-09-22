1ec0f38426c75f914bcc14a146ce8d3faca6aae5
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-22 17:27:59 -0300

    Ant

---
+++ Src/Colony/AntModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AntColony.Colony {
+	/// <summary>
+	/// This is an ant
+	/// </summary>
+	public class AntModel {
+		/// <summary>
+		/// Ant ID
+		/// </summary>
+		public int Id { get; set; }
+	}
+}
