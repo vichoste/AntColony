06171ab32e008477ec0299f61ddd7555c4a059b9
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-23 20:26:30 -0300

    Code clean again

--- Colony/CellModel.cs
+++ Colony/CellModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AntColony.Colony {
+﻿namespace AntColony.Colony {
 	/// <summary>
 	/// This is a cell from the environment
 	/// </summary>
