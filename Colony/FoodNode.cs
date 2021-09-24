02f65868401e0a910f1acde0afd8eb281a31464f
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-24 20:29:22 -0300

    Food node

--- Colony/FoodNode.cs
+++ Colony/FoodNode.cs
@@ -3,12 +3,6 @@
 /// This is an ant
 /// </summary>
 internal class FoodNode : Node {
-	#region Fields
-	/// <summary>
-	/// If this food cell is marked as discovered
-	/// </summary>
-	public bool IsDiscovered { get; set; }
-	#endregion
 	#region Constructors
 	/// <summary>
 	/// Places food
