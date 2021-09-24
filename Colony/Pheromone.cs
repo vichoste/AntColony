c1f4fb69d25c689ee6e1d5c92244b625442d5598
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-24 17:01:14 -0300

    Pheromone update/evaporate

--- Colony/Pheromone.cs
+++ Colony/Pheromone.cs
@@ -5,7 +5,17 @@ internal class Pheromone : Node {
 	/// <summary>
 	/// This value will evaporate over time
 	/// </summary>
-	public byte Strength { get; set; }
+	public double Strength { get; private set; }
+	/// <summary>
+	/// Gets an RGB value for strength
+	/// </summary>
+	public byte ObservableStrength => (byte)(85000 * this.Strength / 333 - 85 / 333);
+	#endregion
+	#region Constructor
+	/// <summary>
+	/// Places a pheromone
+	/// </summary>
+	public Pheromone() => this.Strength = byte.MaxValue;
 	#endregion
 	#region Constants
 	/// <summary>
@@ -16,5 +26,24 @@ internal class Pheromone : Node {
 	/// Maximum evaporation rate
 	/// </summary>
 	public const double MaxPheromoneEvaporationRate = 1;
+	#endregion
+	#region Methods
+	/// <summary>
+	/// Evaporates the pheromone
+	/// </summary>
+	/// <param name="pheromoneEvaporationRate">Pheromone evaporation rate</param>
+	/// <returns>New current strength</returns>
+	public double Evaporate(double pheromoneEvaporationRate) {
+		if (this.Strength - pheromoneEvaporationRate < 0) {
+			this.Strength = 0;
+		} else if (this.Strength > 0) {
+			this.Strength -= pheromoneEvaporationRate;
+		}
+		return this.Strength;
+	}
+	/// <summary>
+	/// Updates the feromone
+	/// </summary>>
+	public void Update() => this.Strength = 1;
 	#endregion
 }
