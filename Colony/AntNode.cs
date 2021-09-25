29335b7c9528bbb8dc3453ab6401cdffce920690
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-25 10:59:24 -0300

    Direction

--- Colony/AntNode.cs
+++ Colony/AntNode.cs
@@ -6,9 +6,6 @@ internal class AntNode : Node {
 	private readonly List<(int X, int Y)> _VisitedCoordinates;
 	public const int MinAntCount = 0;
 	public const int MaxAntCount = 10;
-	public enum Movement {
-		North, South, West, East, NorthWest, NorthEast, SouthWest, SouthEast
-	}
 	public int OriginX { get; set; }
 	public int OriginY { get; set; }
 	public List<(int X, int Y)> VisitedCoordinates => this._VisitedCoordinates.ToList();
---
+++ Colony/Direction.cs
@@ -0,0 +1,4 @@
+﻿namespace AntColony.Colony;
+public enum Direction {
+	North, South, West, East, NorthWest, NorthEast, SouthWest, SouthEast
+}
